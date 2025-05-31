aaf3cc4fdd75a216c24503e9217a654ee75eddb0
qkdqkdzl <bidulgi21@gmail.com>
2025-05-31 15:34:30 +0900

    블록랜덤추가
    블록랜덤으로 추가

---
+++ Assets/Scripts/Player/BlockRandom.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 월드에 블록을 랜덤하게 깔아주는 코드
+/// </summary>
+
+public class BlockRandom : MonoBehaviour
+{
+    [Header("--------- 블록 프리펩 --------")]
+    public GameObject block;
+
+    [Header("--------- 블록 생성 수 --------")]
+    public int spawn;
+
+
+
+
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
+/// <summary>
+/// 플레이어가 계단을 오를 수 있도록 해주는 코드
+/// </summary>
+
 public class Player : MonoBehaviour
 {
     [SerializeField] SpriteRenderer _renderer;
@@ -30,24 +34,35 @@ public class Player : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.J))
         {
+            Debug.Log("회전!");
             Trun();
+            Change();
+            Jump();
         }
     }
     
+    // 캐릭터가 다음 블록으로 점프
     void Jump()
     {
+        // X 좌표 -1.21f 만큼, y 0.65f 만큼 움직임
         position += new Vector2(-1.21f, 0.65f);
         transform.position = position;
     }
 
-    
-    void Trun()
+    // 캐릭터가 돌았을 때 회전시킴
+    void Change()
     {
-        //   , -4.92  , -5.63
-        position += new Vector2(1.33f, 0.71f);
         // flipX를 반대로 전환
         _renderer.flipX = ! _renderer.flipX;
-        Jump();
+        Trun();
+    }
+    
+    // 왼쪽, 오른쪽 방향전환
+    void Trun()
+    {
+        // X 좌표 1.28f 만큼, y 0.71 만큼 움직임
+        position += new Vector2(1.28f, 0.71f);
+        
     }
 
 }
