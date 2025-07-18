01c3ed129e470f81c429df2a15c5603acfc2400b
qkdqkdzl <bidulgi21@gmail.com>
2025-07-18 19:51:24 +0900

    버튼UI수정

--- Assets/Scripts/Player/PlayerBtn.cs
+++ Assets/Scripts/Player/PlayerBtn.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class PlayerBtn : MonoBehaviour
 {
 
-   
-    public GameObject gameOverMessage;   // Canvas 안의 "게임오버" UI 오브젝트
-    public LayerMask blockLayer;         // Block 레이어만 체크
-    
+    private SpriteRenderer playerSpriteRenderer;
+
+
+
     /// <summary>
     /// 버튼 누르면 이동시킬 좌표 값
     /// </summary>
@@ -20,7 +20,7 @@ public class PlayerBtn : MonoBehaviour
     public void OnTrunButton()
     {
         transform.position += new Vector3(1.264f, 0.599f, 0f);
-        
+        //playerSpriteRenderer.flipX = false;
     }
     
 }
